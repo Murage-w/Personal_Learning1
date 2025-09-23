eb0ba2358624b1df7d7ce03083522b21d8ac8b0d
Thriller-W <githinjimurage24@gmail.com>
2025-09-23 12:43:49 +0300

    Arithmetic calculations for the area of a rectangle

---
+++ rectangle_area.py
@@ -0,0 +1,14 @@
+## Calculations for the area of a rectangle.
+
+# Assigning Values.
+
+length = 10
+width = 5
+
+# Calculations for the Area of a Rectangle.
+
+area = length * width
+
+# Results
+
+print (f"The area of the rectangle is: {area}")
