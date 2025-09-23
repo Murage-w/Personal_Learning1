42b4d409ef0399a3c3c6b34767ccc0e65955292c
Thriller-W <githinjimurage24@gmail.com>
2025-09-23 11:50:46 +0300

    Basic arithmetic operations in python

---
+++ basic_operations.py
@@ -0,0 +1,19 @@
+## The practice of basic arithmetic operations in python by performing predefined calculations.
+
+#Assign Values
+
+number1 = 10
+number2 = 5
+
+# Calculations
+
+addition = number1 + number2
+substraction = number1 - number2
+multiplication = number1 * number2
+
+# The outcome/ Results
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Substraction of {number1} and {number2} is {substraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
+
