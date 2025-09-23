67df036a79de44cbd9b2f21184474ce28b05988e
Thriller-W <githinjimurage24@gmail.com>
2025-09-23 12:33:59 +0300

    Simple Interest Calculator

---
+++ simple_interest.py
@@ -0,0 +1,16 @@
+## Calculation of Simple Interest earned on an Investment over a period of time.
+
+# Assigning Values
+
+principal = 1000
+rate = 0.05
+time = 3
+
+# Simple Interest Calculations
+
+interest = principal * rate * time
+
+# Results
+
+print (f"The simple interest is: {interest}")
+
