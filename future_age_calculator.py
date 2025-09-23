4ce9bd87802df1204ce9ba404d81b5a74bf8acd8
Thriller-W <githinjimurage24@gmail.com>
2025-09-23 13:18:17 +0300

    Future age Calculator

---
+++ future_age_calculator.py
@@ -0,0 +1,18 @@
+## Receiving User Input and Calculating their future age.
+
+# Ask for current age
+
+current_age = int(input(f"How old are you? "))
+
+# Years until 2050
+
+years_until_2050 = 2050 - 2023
+
+
+# Calculations for age
+
+age = current_age + years_until_2050
+
+# Results
+
+print(f"In 2050, you will be {age} years old.")
