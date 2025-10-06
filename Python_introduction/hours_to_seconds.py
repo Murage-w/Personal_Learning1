0c4d5563416c905fe0b18837520c179efa04d8dd
Thriller-W <githinjimurage24@gmail.com>
2025-10-06 17:06:35 +0300

    Moving  all files to a new directory

---
+++ Python_introduction/hours_to_seconds.py
@@ -0,0 +1,14 @@
+## Conversion of Hours to Seconds
+
+# Number of Hours
+
+hours = 2
+
+# Hours to seconds conversion
+
+seconds = 2 * 3600
+
+# Results 
+
+print(f"{hours} hours is {seconds} seconds.")
+
